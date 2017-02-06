0e8ca3ad3adb0da8c844d3cc3bc079263a658bcd
Raúl Blanco <rbr007.movil@gmail.com>
2017-02-06 20:17:33 +0100

    Inicio del proyecto

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Prueba de despliegue desde Github</title>
+  </head>
+  <body>
+    <h1>Despliegue desde Github</h1>
+    <?php phpinfo(); ?>
+  </body>
+</html>
